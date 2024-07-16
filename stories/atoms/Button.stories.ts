b5ab93ae14643a21167fd572cf31efc6f8e8074a
O6lvl4 <hiromi.motodera@aid-on.org>
2024-07-16 18:39:28 +0900

    Update storybook

--- stories/atoms/Button.stories.ts
+++ stories/atoms/Button.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-import { Button } from '../src/components/atoms/Button';
+import { Button } from '../../src/components/atoms/Button';
 
 const meta = {
   title: 'Atoms/Button',
--- stories/atoms/SideLogo.stories.tsx
+++ stories/atoms/SideLogo.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meta, StoryFn } from '@storybook/react';
-import { SideLogo } from '../src/components/atoms/SideLogo';
+import { SideLogo } from '../../src/components/atoms/SideLogo';
 import { Box } from '@chakra-ui/react';
 
 export default {
